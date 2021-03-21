598397295d67578c531e4bf57bc3f3dcca284ab8
Cooper Saye <coopersaye@gmail.com>
2021-03-21 07:46:20 -0600

    Import quine programs.

---
+++ quines/ArrayQuine.py
@@ -0,0 +1,12 @@
+s=[
+'s=[',
+'print(s[0])',
+'for i in range(0, 6): print(chr(39) + s[i] + chr(39) + chr(44))',
+'print(s[5])',
+'for i in range(1, 5): print(s[i])',
+']',
+]
+print(s[0])
+for i in range(0, 6): print(chr(39) + s[i] + chr(39) + chr(44))
+print(s[5])
+for i in range(1, 5): print(s[i])
---
+++ quines/DoubleQuine.py
@@ -0,0 +1,18 @@
+s=[
+'# 1',
+'# 2',
+'s=[',
+'print(s[0]);print(s[4])',
+'print(s[1]);print(s[3])',
+'print(s[2])',
+'for l in s: print(chr(39) + l + chr(39) + chr(44))',
+'print(s[9])',
+'for i in range(5, 9): print(s[i])',
+']',
+]
+print(s[2])
+for l in s: print(chr(39) + l + chr(39) + chr(44))
+print(s[9])
+for i in range(5, 9): print(s[i])
+# 1
+print(s[1]);print(s[3])
